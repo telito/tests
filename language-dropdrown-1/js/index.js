b1d3e44265761675cc2ba156fc03712c89ee6042
Daniel Ramos de Carvalho <danielramosbh74@gmail.com>
2020-05-11 07:30:36 -0300

    Create index.js

---
+++ language-dropdrown-1/js/index.js
@@ -0,0 +1,13 @@
+/* I've tried to separate JavaScript, but it did'n work...
+
+<script type="text/javascript">
+function changeLanguage(lang) {
+    document.getElementById("content").innerHTML = document.getElementById(lang).innerHTML;
+}
+
+</script>
+<script type="text/javascript">
+document.getElementById("content").innerHTML = document.getElementById("english").innerHTML;
+</script>
+
+*/
